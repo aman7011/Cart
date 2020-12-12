9121b0410fdc8cfbc0fc33e77515fd9053d79a57
aman7011 <chaturvediaman04@gmail.com>
2020-12-12 15:00:02 +0530

    done cart functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,110 @@
+import React from 'react';
+import Cart from './Cart';
+import Navbar from './Navbar';
 
-import React from 'react'
-import CartItem from './CartItem';
-function App() {
+class App extends React.Component {
 
-  return (
-    <div className="App">
-      
-       <CartItem />
-    </div>
-  );
+  constructor () {
+    super();
+    this.state = {
+      products: [
+        {
+          price: 99,
+          title: 'Watch',
+          qty: 2,
+          img: 'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8d2F0Y2h8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+          id: 1
+        },
+        {
+          price: 9999,
+          title: 'Mobile Phone',
+          qty: 1,
+          img: 'https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80',
+          id: 2
+        },
+        {
+          price: 999,
+          title: 'speaker',
+          qty: 1,
+          img: 'https://images.unsplash.com/photo-1545454675-3531b543be5d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8c3BlYWtlcnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+          id: 3
+        }
+      ]
+    }
+    // this.increaseQuantity = this.increaseQuantity.bind(this);
+    // this.testing();
+  }
+  handleIncreaseQuantity = (product) => {
+    console.log('Heyy please inc the qty of ', product);
+    const { products } = this.state;
+    const index = products.indexOf(product);
+
+    products[index].qty += 1;
+
+    this.setState({
+      products
+    })
+  }
+  handleDecreaseQuantity = (product) => {
+    console.log('Heyy please inc the qty of ', product);
+    const { products } = this.state;
+    const index = products.indexOf(product);
+
+    if (products[index].qty === 0) {
+      return;
+    }
+
+    products[index].qty -= 1;
+
+    this.setState({
+      products
+    })
+  }
+  handleDeleteProduct = (id) => {
+    const { products } = this.state;
+
+    const items = products.filter((item) => item.id !== id); // [{}]
+
+    this.setState({
+      products: items
+    })
+  }
+
+  getCartCount = () => {
+    const { products } = this.state;
+
+    let count = 0;
+
+    products.forEach((product) => {
+      count += product.qty;
+    })
+
+    return count;
+  }
+ getCartTotal = () =>{
+   const {products} = this.state;
+   let cartTotal =0;
+   products.map((product) => {
+   cartTotal = cartTotal + product.qty * product.price
+   })
+   return cartTotal;
+ }
+  render () {
+    const { products } = this.state;
+    return (
+      <div className="App">
+        <Navbar count={this.getCartCount()} />
+        <Cart
+          products={products}
+          onIncreaseQuantity={this.handleIncreaseQuantity}
+          onDecreaseQuantity={this.handleDecreaseQuantity}
+          onDeleteProduct={this.handleDeleteProduct}
+        />
+        <div style= {{padding:10,  fontSize: 20}}>TOTAL: {this.getCartTotal()}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
