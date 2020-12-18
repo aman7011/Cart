6fec502adcbea7dce9ad15ada3dd1906359a1ae3
aman7011 <chaturvediaman04@gmail.com>
2020-12-18 15:55:15 +0530

    firebase added and updated

--- src/App.js
+++ src/App.js
@@ -1,38 +1,56 @@
 import React from 'react';
 import Cart from './Cart';
 import Navbar from './Navbar';
+import firebase from 'firebase/app'
+
 
 class App extends React.Component {
 
   constructor () {
     super();
     this.state = {
-      products: [
-        {
-          price: 99,
-          title: 'Watch',
-          qty: 2,
-          img: 'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8d2F0Y2h8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-          id: 1
-        },
-        {
-          price: 9999,
-          title: 'Mobile Phone',
-          qty: 1,
-          img: 'https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80',
-          id: 2
-        },
-        {
-          price: 999,
-          title: 'speaker',
-          qty: 1,
-          img: 'https://images.unsplash.com/photo-1545454675-3531b543be5d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8c3BlYWtlcnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-          id: 3
-        }
-      ]
+      products: [],
+      loading : true
     }
-    // this.increaseQuantity = this.increaseQuantity.bind(this);
-    // this.testing();
+  }
+  componentDidMount(){
+  //  firebase
+  //  .firestore()
+  //  .collection('products')
+  //  .get()
+  //  .then((snapshot) =>{
+  //    console.log(snapshot);
+  //    snapshot.docs.map((doc) =>{
+  //      console.log(doc.data())
+  //    });
+  //    const products= snapshot.docs.map((doc) => {
+  //      const data= doc.data();
+  //      data ['id']= doc.id;
+  //      return data;
+  //    })
+  //    this.setState({
+  //      products,
+  //      loading: false
+  //    })
+  //  })
+  firebase
+  .firestore()
+  .collection('products')
+  .onSnapshot((snapshot) =>{
+     console.log(snapshot);
+       snapshot.docs.map((doc) =>{
+         console.log(doc.data())
+       });
+       const products= snapshot.docs.map((doc) => {
+         const data= doc.data();
+         data ['id']= doc.id;
+         return data;
+       })
+       this.setState({
+         products,
+         loading: false
+       })
+     })
   }
   handleIncreaseQuantity = (product) => {
     console.log('Heyy please inc the qty of ', product);
@@ -85,12 +103,15 @@ class App extends React.Component {
    const {products} = this.state;
    let cartTotal =0;
    products.map((product) => {
-   cartTotal = cartTotal + product.qty * product.price
-   })
+  if(product.qty> 0){
+    cartTotal = cartTotal + product.qty * product.price
+  } 
+  return ';'
+  });
    return cartTotal;
  }
   render () {
-    const { products } = this.state;
+    const { products, loading  } = this.state;
     return (
       <div className="App">
         <Navbar count={this.getCartCount()} />
@@ -100,6 +121,7 @@ class App extends React.Component {
           onDecreaseQuantity={this.handleDecreaseQuantity}
           onDeleteProduct={this.handleDeleteProduct}
         />
+        {loading && <h1>Loading Products...</h1>}
         <div style= {{padding:10,  fontSize: 20}}>TOTAL: {this.getCartTotal()}</div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 import App from './App';
+import firebase from 'firebase/app'
+import 'firebase/firestore';
 
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+    apiKey: "AIzaSyB_CP0h_ingl9U3lWhffo6EZVqOeRlwRW0",
+    authDomain: "cart-27c6d.firebaseapp.com",
+    projectId: "cart-27c6d",
+    storageBucket: "cart-27c6d.appspot.com",
+    messagingSenderId: "492252990612",
+    appId: "1:492252990612:web:56c4eb643b78865a46b881"
+  };
+  // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
     <App />, document.getElementById('root')
